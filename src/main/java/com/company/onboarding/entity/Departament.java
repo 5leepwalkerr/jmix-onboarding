db50a1bb092e0e5b7a0e873b364a23b7ed276804
awesomemax777 <cvb.cvb.2017.cvb@gmail.com>
2023-09-13 22:45:07 +0300

    new remote origin

--- src/main/java/com/company/onboarding/entity/Departament.java
+++ src/main/java/com/company/onboarding/entity/Departament.java
@@ -3,6 +3,8 @@ package com.company.onboarding.entity;
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -16,6 +18,10 @@ import java.util.UUID;
 })
 @Entity
 public class Departament {
+    public Departament(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
     @JmixGeneratedValue
     @Column(name = "ID", nullable = false)
     @Id
@@ -25,52 +31,4 @@ public class Departament {
     @Column(name = "name", length = 50)
     @NotNull
     private String name;
-
-    @JoinColumn(name = "hr_manger_Id")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User hrManager;
-
-    @Column(name = "VERSION", nullable = false)
-    @Version
-    private Integer version;
-
-    public void setHrManager(User hrManager) {
-        this.hrManager = hrManager;
-    }
-
-    public User getHrManager() {
-        return hrManager;
-    }
-
-    public User getHtManager() {
-        return hrManager;
-    }
-
-    public void setHtManager(User htManager) {
-        this.hrManager = htManager;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
 }
